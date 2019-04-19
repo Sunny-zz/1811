90376151a2ec9006fb6cad764e60916b46619ad8
Sunny-zz <1348369672@qq.com>
2019-04-19 12:01:30 +0800

    vue-router-demo

---
+++ vue-router-demo/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- 文章列表 -->
+  <div>
+    <ul>
+      <li>第一篇</li>
+      <li>第二篇</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app"
+};
+</script>
+
+<style>
+</style>
---
+++ vue-router-demo/src/components/Article.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>文章详情</div>
+</template>
+
+<script>
+export default {
+  name: "articleInfo"
+};
+</script>
+
+<style>
+</style>
