a5b5f4814252deb098287c19a3a7dcd11248a3dc
Sunny-zz <1348369672@qq.com>
2019-04-23 10:43:06 +0800

    vue-juejin nav-link

--- vue-juejin/src/components/Header.vue
+++ vue-juejin/src/components/Header.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="header">
     <img src="https://b-gold-cdn.xitu.io/v3/static/img/logo.a7995ad.svg" alt>
-    <router-link active-class="active-nav" to="/" exact>首页</router-link>
+    <!-- 当链接为 welcome/xxx 的时候我的首页link的匹配规则需是不严格匹配，而其他情况需要严格匹配 -->
+    <!-- 获取页面的当前路径 通过路由的一些属性 -->
+    <!-- vue 路由我们有两种获取路由信息的属性 -->
+    <!-- 1. 获取当前页面的路由信息 this.$route 在 template 内直接使用 $route -->
+    <!-- 2. 获取整个路由的信息 this.$router 在 template 内直接使用 $router -->
+    <router-link active-class="active-nav" to="/" :exact="isExact">首页</router-link>
     <router-link active-class="active-nav" to="/pins">沸点</router-link>
     <router-link active-class="active-nav" to="topics">话题</router-link>
     <!-- 链接激活时 就是页面的地址包含链接的地址时，就叫激活 可以使用 exact 将激活规则变成严格模式-->
@@ -11,7 +16,12 @@
 
 <script>
 export default {
-  name: "topheader"
+  name: "topheader",
+  computed: {
+    isExact() {
+      return this.$route.path.indexOf("welcome") === -1 ? true : false;
+    }
+  }
 };
 </script>
 
--- vue-juejin/src/components/Home.vue
+++ vue-juejin/src/components/Home.vue
@@ -1,8 +1,9 @@
 <template>
-  <div>
+  <div class="home">
     <ul>
       <li>
-        <router-link to="/welcome/recommended">推荐</router-link>
+        <!-- router-link 跳转页面因为 to 属性内写的地址 会被传递到 路由一个 push 方法内 -->
+        <router-link to="/welcome/recommended" :class="isHome? 'router-link-active': ''">推荐</router-link>
       </li>
       <li>
         <router-link to="/welcome/backend">后端</router-link>
@@ -17,9 +18,28 @@
 
 <script>
 export default {
-  name: "home"
+  name: "home",
+  computed: {
+    isHome() {
+      return this.$route.fullPath === "/";
+    }
+  }
 };
 </script>
 
 <style>
+.home ul {
+  display: flex;
+  list-style: none;
+  padding: 0;
+}
+.home ul li a {
+  text-decoration: none;
+  color: darkgray;
+  margin-right: 20px;
+}
+
+.home ul li .router-link-active {
+  color: aqua;
+}
 </style>
