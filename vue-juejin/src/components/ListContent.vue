04816251ffb2bb0698d115c27f4c0d711e7c8893
Sunny-zz <1348369672@qq.com>
2019-04-23 16:52:02 +0800

    vue-juejin data  ajax watch done

--- vue-juejin/src/components/ListContent.vue
+++ vue-juejin/src/components/ListContent.vue
@@ -21,6 +21,10 @@ export default {
     // 监听的对象内 属性名加引号可以直接写 js 而且想要获取该组件相关的内容是不惜要加 this
     "$route.path": function() {
       // 根据页面的地址获取对应的数据更新 data
+      const { type } = this.$route.params;
+      axios.get(`http://localhost:3008/posts?postType=${type}`).then(res => {
+        this.posts = res.data;
+      });
     }
   },
   created() {
@@ -29,9 +33,19 @@ export default {
     // 刷新页面的时候要获取推荐型的文章
     // 传参方式
     // 1. 写在地址后面 ？后
-    axios.get("http://localhost:3008/posts?isRecommended=true").then(res => {
-      this.posts = res.data;
-    });
+    const { type } = this.$route.params;
+    console.log(type);
+    // type 当切到 recommended 路由是没路有参数 获取的是 undefined
+    // 当不是 undefined 时，请求的参数就需要 postType=${type}
+    axios
+      .get(
+        `http://localhost:3008/posts?${
+          !type ? "isRecommended=true" : `postType=${type}`
+        }`
+      )
+      .then(res => {
+        this.posts = res.data;
+      });
   }
 };
 </script>
