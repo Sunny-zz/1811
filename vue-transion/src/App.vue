93e0f64b28627506405d55a309ada39c5203ef95
Sunny-zz <1348369672@qq.com>
2019-04-24 15:45:00 +0800

    vue-transition shuffle

--- vue-transion/src/App.vue
+++ vue-transion/src/App.vue
@@ -8,19 +8,34 @@
     <div>
       <input type="text" v-model="val">
       <button @click="add">添加</button>
-      <ul>
-        <transition-group name="list" appear>
-          <li v-for="user in users" :key="user.id">
-            {{user.username}}
-            <button @click="del(user.id)">删除</button>
-          </li>
-        </transition-group>
-      </ul>
+      <button @click="del">删除</button>
+      <button @click="shuffle">随机打乱</button>
+
+      <!-- transition  以及 transition-group 组件 vue 本身自带的动画组件 -->
+      <!-- 1. transition 不会渲染成标签 -->
+      <!-- 2. transition-group 会默认渲染成 span 标签  使用 tag 属性更改-->
+      <transition-group name="list" appear tag="ul">
+        <li class="list-item" v-for="user in users" :key="user.id">{{user.username}}</li>
+      </transition-group>
     </div>
   </div>
 </template>
 
 <script>
+// 已知一个数组，将其顺序打乱
+// const arr = [1, 2, 3, 4, 5];
+// let newArr = [];
+// // 0 1 2 3 4
+// for (let index = 0; index < arr.length; index++) {
+//   const num = Math.floor(Math.random() * arr.length);
+//   const val = arr[num];
+//   newArr.push(val);
+//   index--;
+//   arr.splice(num, 1);
+//   console.log(val);
+// }
+// console.log(newArr);
+import _ from "lodash";
 export default {
   name: "app",
   data() {
@@ -43,12 +58,22 @@ export default {
     add() {
       const val = this.val;
       if (val.trim()) {
-        this.users.push({
+        this.users.splice(this.randomIndex(), 0, {
           id: new Date().getTime(),
           username: val
         });
         this.val = "";
       }
+    },
+    del() {
+      // this.users = this.users.filter(user => user.id != id);
+      this.users.splice(this.randomIndex() - 1, 1);
+    },
+    randomIndex() {
+      return Math.floor(Math.random() * (this.users.length + 1));
+    },
+    shuffle() {
+      this.users = _.shuffle(this.users);
     }
   }
 };
@@ -93,12 +118,25 @@ export default {
   }
 }
 
-.list-enter-active,
+/* .list-enter-active,
 .list-leave-active {
-  transition: all 2s;
+  transition: all 1s;
 }
 .list-enter,
 .list-leave-to {
   opacity: 0;
+} */
+.list-item {
+  transition: all 1s;
+  display: inline-block;
+  margin-right: 20px;
+}
+.list-enter, .list-leave-to
+/* .list-leave-active for below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
+.list-leave-active {
+  position: absolute;
 }
 </style>
