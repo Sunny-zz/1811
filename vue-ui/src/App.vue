c9b2070b411045f64350ccc07a362250b5bee7e4
Sunny-zz <1348369672@qq.com>
2019-04-25 11:29:37 +0800

    vue-ui progress setInterval and clearInterval

--- vue-ui/src/App.vue
+++ vue-ui/src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <el-button>默认按钮</el-button>
     <el-progress :text-inside="true" :stroke-width="18" :percentage="percent"></el-progress>
-    <button @click="percent = 50">按钮</button>
+    <button @click="handleClick">按钮</button>
     <Test>展示app的字</Test>
   </div>
 </template>
@@ -20,11 +20,23 @@ export default {
     Test
   },
   created() {
-    setInterval(() => {
+    // 如何在 vue 的组件中设置一个全局(该组件)的变量
+    // 直接  this.xxx = xxx
+    this.changePercent = setInterval(() => {
+      console.log("1");
       if (this.percent < 50) {
         this.percent++;
+      } else {
+        // 停止定时器
+        clearInterval(this.changePercent);
       }
     }, 10);
+  },
+  methods: {
+    handleClick() {
+      // 停止定时器
+      clearInterval(this.changePercent);
+    }
   }
 };
 </script>
