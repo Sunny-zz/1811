8256d3f408ae0d5ab8193a4015dbe46a981b1e9f
Sunny-zz <1348369672@qq.com>
2019-04-25 14:46:52 +0800

    vue-ui pagination

--- vue-ui/src/App.vue
+++ vue-ui/src/App.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>
+  <div class="wrap">
     <el-button>默认按钮</el-button>
     <el-progress :text-inside="true" :stroke-width="18" :percentage="percent"></el-progress>
     <button @click="handleClick">按钮</button>
     <Test>展示app的字</Test>
+    <i class="el-icon-edit"></i>
+    <el-button type="primary" icon="el-icon-search">搜索</el-button>
+    <el-switch v-model="value1" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+    <span v-if="value1">年付</span>
+    <span v-else>月付</span>
+    <el-pagination
+      background
+      layout="prev, jumper,pager, next "
+      :total="150"
+      :page-size="15"
+      :current-page="1"
+      next-text="下一页"
+      @current-change="changePage"
+    ></el-pagination>
+    <!-- 点击某一个的时候 必须得知道 点的是第几页，而且点击页码的时候做一些事情 -->
   </div>
 </template>
 
@@ -13,7 +28,8 @@ export default {
   name: "app",
   data() {
     return {
-      percent: 0
+      percent: 0,
+      value1: true
     };
   },
   components: {
@@ -36,9 +52,20 @@ export default {
     handleClick() {
       // 停止定时器
       clearInterval(this.changePercent);
+    },
+    changePage(val) {
+      console.log("点了页码" + val);
+      // 根据页码发请求获取数据更新页面
     }
   }
 };
 </script>
 <style>
+.wrap .el-pagination.is-background .el-pager li:not(.disabled).active {
+  background-color: teal;
+  color: pink;
+}
+.wrap .el-pagination.is-background .el-pager li:not(.disabled):hover {
+  color: brown;
+}
 </style>
--- vue-ui/src/element-component.js
+++ vue-ui/src/element-component.js
@@ -1,5 +1,7 @@
 // 专门用来引入 element 组件
 import Vue from "vue"
-import { Button, Progress } from "element-ui"
+import { Button, Progress, Switch, Pagination } from "element-ui"
 Vue.component(Button.name, Button)
 Vue.component(Progress.name, Progress)
+Vue.component(Switch.name, Switch)
+Vue.component(Pagination.name, Pagination)
