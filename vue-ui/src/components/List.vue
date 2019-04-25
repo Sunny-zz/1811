8a4681b15853477c12d1b5232f7c851dbef0041c
Sunny-zz <1348369672@qq.com>
2019-04-25 16:14:52 +0800

    vue-ui pagination demo 分页

--- vue-ui/src/components/List.vue
+++ vue-ui/src/components/List.vue
@@ -1,10 +1,43 @@
 <template>
-  <div>list</div>
+  <div v-if="posts">
+    <ul>
+      <li v-for="post in showPosts" :key="posts.id">{{post.title}}</li>
+    </ul>
+    <!-- 分页器 -->
+    <el-pagination
+      background
+      layout="prev,pager, next ,jumper"
+      :total="posts.length"
+      :page-size="10"
+    ></el-pagination>
+  </div>
+  <div v-else>请稍等...</div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "list"
+  name: "list",
+  data() {
+    return {
+      posts: null
+      // pageSize: 1
+    };
+  },
+  computed: {
+    showPosts() {
+      // 0 - 10    10  20
+      return this.posts.slice(0, 10);
+    }
+  },
+  created() {
+    // 发送请求获取数据
+
+    axios.get("http://jsonplaceholder.typicode.com/posts").then(res => {
+      this.posts = res.data;
+    });
+  }
 };
 </script>
 
