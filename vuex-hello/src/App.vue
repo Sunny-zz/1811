16588509e1c3def92ffe3b9d4e0d8e33c7b23afb
Sunny-zz <1348369672@qq.com>
2019-05-07 15:14:45 +0800

    vuex-hello mapMutations

--- vuex-hello/src/App.vue
+++ vuex-hello/src/App.vue
@@ -1,21 +1,33 @@
 <template>
   <div>
     <h1>Vuex demo</h1>
-    <span>{{$store.state.num}}</span>
+    <span>{{$store.state.obj.num}}</span>
     <br>
-    <!-- <button @click="$store.commit('add')">+</button>
-    <button @click="$store.commit('sub')">-</button>
-    <button @click="$store.commit('change',10000)">替换</button>-->
+    <button @click="add">+</button>
+    <button @click="sub">-</button>
+    <button @click="change(1000)">替换</button>
     <!-- 对象类型的提交 mutation -->
-    <button @click="$store.commit({type:'add'})">+</button>
+    <!-- <button @click="$store.commit({type:'add'})">+</button>
     <button @click="$store.commit({type:'sub'})">-</button>
-    <button @click="$store.commit({type:'change',newNum: 10000})">替换</button>
+    <button @click="$store.commit({type:'change',newNum: 10000})">替换</button>-->
   </div>
 </template>
 
 <script>
+// 使用 mapMutations 简化  $store.commit()
+
+import { mapMutations } from "vuex";
 export default {
-  name: "app"
+  name: "app",
+  methods: {
+    // 将 store 中的 mutation 映射成组件的方法
+    // ...mapMutations({
+    //   addX: "add",
+    //   sub: "sub",
+    //   change: "change"
+    // }),
+    ...mapMutations(["add", "sub", "change"])
+  }
 };
 </script>
 
--- vuex-hello/src/store/index.js
+++ vuex-hello/src/store/index.js
@@ -2,7 +2,7 @@ import Vue from "vue"
 import Vuex from "vuex"
 Vue.use(Vuex)
 
-// 创建 store
+// 创建 store(存储共享状态的容器)
 const store = new Vuex.Store({
   // 有一个属性 state 该属性就是存储共享数据的 数据类型是对象类型
   // 修改 store 的逻辑
@@ -10,24 +10,24 @@ const store = new Vuex.Store({
   // 需要使用 store 的 commit 方法触发 mutation 进而动态修改 store
   state: {
     obj: {
-      num: 10
+      num: 0
     }
   },
   mutations: {
     // 放的是修改 state 的函数(方法)
     // 函数默认第一个参数就是 state，直接对 state 下的数据进行修改，就是修改了 store
     add(state) {
-      state.num++
+      state.obj.num++
     },
     sub(state) {
-      if (state.num > 1) {
-        state.num--
+      if (state.obj.num > 1) {
+        state.obj.num--
       }
     },
     // mutation 接受额外的参数
     // commit 如何传递参数
-    change(state, payload) {
-      state.num = payload.newNum
+    change(state, num) {
+      state.obj.num = num
     }
   }
 })
