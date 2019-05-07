cb0cd7b8640997df715feade1932e671195eea01
Sunny-zz <1348369672@qq.com>
2019-05-07 11:59:11 +0800

    vuex-hello store commit

--- vuex-hello/src/App.vue
+++ vuex-hello/src/App.vue
@@ -3,7 +3,13 @@
     <h1>Vuex demo</h1>
     <span>{{$store.state.num}}</span>
     <br>
-    <button @click="$store.commit('add')">+</button>
+    <!-- <button @click="$store.commit('add')">+</button>
+    <button @click="$store.commit('sub')">-</button>
+    <button @click="$store.commit('change',10000)">替换</button>-->
+    <!-- 对象类型的提交 mutation -->
+    <button @click="$store.commit({type:'add'})">+</button>
+    <button @click="$store.commit({type:'sub'})">-</button>
+    <button @click="$store.commit({type:'change',newNum: 10000})">替换</button>
   </div>
 </template>
 
--- vuex-hello/src/store/index.js
+++ vuex-hello/src/store/index.js
@@ -9,13 +9,25 @@ const store = new Vuex.Store({
   // 需要定义 mutation 更新 state
   // 需要使用 store 的 commit 方法触发 mutation 进而动态修改 store
   state: {
-    num: 10
+    obj: {
+      num: 10
+    }
   },
   mutations: {
     // 放的是修改 state 的函数(方法)
     // 函数默认第一个参数就是 state，直接对 state 下的数据进行修改，就是修改了 store
     add(state) {
       state.num++
+    },
+    sub(state) {
+      if (state.num > 1) {
+        state.num--
+      }
+    },
+    // mutation 接受额外的参数
+    // commit 如何传递参数
+    change(state, payload) {
+      state.num = payload.newNum
     }
   }
 })
