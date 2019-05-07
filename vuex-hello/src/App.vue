b4399ba7be146d9ad3e81e6c00a31535b8616d0e
Sunny-zz <1348369672@qq.com>
2019-05-07 11:09:41 +0800

    vuex-hello store

--- vuex-hello/src/App.vue
+++ vuex-hello/src/App.vue
@@ -2,15 +2,14 @@
   <div>
     <h1>Vuex demo</h1>
     <span>{{$store.state.num}}</span>
+    <br>
+    <button @click="$store.commit('add')">+</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: "app",
-  created() {
-    console.log(this);
-  }
+  name: "app"
 };
 </script>
 
--- vuex-hello/src/store/index.js
+++ vuex-hello/src/store/index.js
@@ -5,9 +5,18 @@ Vue.use(Vuex)
 // 创建 store
 const store = new Vuex.Store({
   // 有一个属性 state 该属性就是存储共享数据的 数据类型是对象类型
+  // 修改 store 的逻辑
+  // 需要定义 mutation 更新 state
+  // 需要使用 store 的 commit 方法触发 mutation 进而动态修改 store
   state: {
-    num: 10,
-    a: 20
+    num: 10
+  },
+  mutations: {
+    // 放的是修改 state 的函数(方法)
+    // 函数默认第一个参数就是 state，直接对 state 下的数据进行修改，就是修改了 store
+    add(state) {
+      state.num++
+    }
   }
 })
 
