0d791d62ee5284a113cd36640ec2b68ab4de92b2
Sunny-zz <1348369672@qq.com>
2019-05-10 17:32:11 +0800

    0510

---
+++ vue-production/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>hello</h1>
+    <button @click="num--">-</button>
+    <span>{{num}}</span>
+    <button @click="num++">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      num: 0
+    };
+  }
+};
+</script>
+
+<style>
+</style>
