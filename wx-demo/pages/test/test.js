921241c27d581646a78a0e31f0176d1309329c5a
Sunny-zz <1348369672@qq.com>
2019-05-24 14:24:45 +0800

    wx-demo update

--- wx-demo/pages/test/test.js
+++ wx-demo/pages/test/test.js
@@ -32,7 +32,11 @@ Page({
         age: 30
       }
     ],
-    imgUrls: []
+    imgUrls: [
+      "https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640",
+      "https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640",
+      "https://images.unsplash.com/photo-1551446591-142875a901a1?w=640"
+    ]
   },
   // 256  127
   // 600rpx  ?
@@ -97,7 +101,7 @@ Page({
    */
   onLoad: function() {
     wx.request({
-      url: "https://przpn06co.bkt.clouddn.com/data.json",
+      url: "https://cnodejs.org/api/v1/topics?limit=5",
       success(res) {
         console.log(res.data)
       }
